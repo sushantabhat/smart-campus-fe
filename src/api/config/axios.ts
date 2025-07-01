9a95763c578d3e2f47e2bc2980028692aee6d307
sanjayguwaju <sanjayguwaju@gmail.com>
2025-07-01 07:39:08 +0545

    chore: update devDependencies to include @types/node and adjust Vite configuration to load environment variables

--- src/api/config/axios.ts
+++ src/api/config/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { useAuthStore } from '../../store/authStore';
 
-const API_BASE_URL = 'http://localhost:5001/api/v1';
+const API_BASE_URL = import.meta.env.VITE_BACKEND_API_BASE_URL;
 
 export const apiClient = axios.create({
   baseURL: API_BASE_URL,
